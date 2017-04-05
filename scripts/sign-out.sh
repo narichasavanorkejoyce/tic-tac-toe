031b6ce042f95a10bed7a05e38affcdb3b7e7653
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-05 10:35:10 -0400

    Remove token and ID reference in sign-out.sh

--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -13,8 +13,8 @@
 
 
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/sign-out/9"
+URL_PATH="/sign-out/ID"
 curl "${API}${URL_PATH}" \
   --include \
   --request DELETE \
-  --header "Authorization: Token token=BAhJIiUwMzFlOGQ2YzBjY2Q1Mzc3ODUyMWJmNTk3M2JhZDllNwY6BkVG--0645648640be90a047107c16585147440f7e169e"
+  --header "Authorization: Token token=token"
