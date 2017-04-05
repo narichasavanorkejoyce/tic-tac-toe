5097cd2b059b93107586b1bc2fc07fc4fd02dd9a
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-05 15:45:43 -0400

    Changed config.apiOrigins to config.apiOrigin

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -8,7 +8,7 @@ const config = require('../config')
 const signUp = function (data) {
   console.log(data)
   return $.ajax({
-    url: config.apiOrigins + '/sign-up/',
+    url: config.apiOrigin + '/sign-up/',
     method: 'POST',
     data
   })
