7979ec476a9258c3e81b6fce75ed5dca51512c27
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-06 11:48:53 -0400

    Wrote curl request for CREATE action
    This curl request resulted in a 201, created response

--- assets/scripts/gameAuth/curlRequests/create.sh
+++ assets/scripts/gameAuth/curlRequests/create.sh
@@ -1,12 +1,10 @@
-{
-  "game": {
-    "id": 3,
-    "cells": ["","","","","","","","",""],
-    "over": false,
-    "player_x": {
-      "id": 1,
-      "email": "and@and.com"
-    },
-    "player_o": null
-  }
-}
+#!/bin/bash
+# sh assets/scripts/gameAuth/curlRequests/create.sh
+
+curl "http://localhost:4741/games" \
+  --include \
+  --request POST \
+  --header "Authorization: Token token=BAhJIiVhMGNiOTQ2MDAzYmEzMjk0YzA4ZjJiMzA3OTc5ZjQwNgY6BkVG--cc1919422e6c9548f6c38db71ac66170b8211310"
+
+# data output from curl doesn't have a trailing newline
+echo
