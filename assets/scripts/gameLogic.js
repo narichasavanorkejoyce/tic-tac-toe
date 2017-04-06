51eecafab80ff7ef357e885df57aa200c5f13e01
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-06 14:49:34 -0400

    Populate input form with index, marker, outcome
    This input form will be populated after each move.
    This data will ultimately be sent up through the
    PATCH AJAX request.

--- assets/scripts/gameLogic.js
+++ assets/scripts/gameLogic.js
@@ -1,28 +1,39 @@
 const checkWin = function (slotValue) {
   if ($('#0').html() !== '' && $('#0').html() === $('#1').html() && $('#0').html() === $('#2').html()) {
     // location.reload()
+    $('#game-outcome').val('true')
     return console.log('You found a match!')
   } else if ($('#0').html() !== '' && $('#0').html() === $('#3').html() && $('#0').html() === $('#6').html()) {
     // location.reload()
+    $('#game-outcome').val('true')
     return console.log('You found a match!')
   } else if ($('#0').html() !== '' && $('#0').html() === $('#4').html() && $('#0').html() === $('#8').html()) {
     // location.reload()
+    $('#game-outcome').val('true')
     return console.log('You found a match!')
   } else if ($('#2').html() !== '' && $('#2').html() === $('#4').html() && $('#2').html() === $('#6').html()) {
     // location.reload()
+    $('#game-outcome').val('true')
     return console.log('You found a match!')
   } else if ($('#1').html() !== '' && $('#1').html() === $('#4').html() && $('#1').html() === $('#7').html()) {
     // location.reload()
+    $('#game-outcome').val('true')
     return console.log('You found a match!')
   } else if ($('#2').html() !== '' && $('#2').html() === $('#5').html() && $('#2').html() === $('#8').html()) {
     // location.reload()
+    $('#game-outcome').val('true')
     return console.log('You found a match!')
   } else if ($('#6').html() !== '' && $('#6').html() === $('#7').html() && $('#6').html() === $('#8').html()) {
     // location.reload()
+    $('#game-outcome').val('true')
     return console.log('You found a match!')
   } else if ($('#3').html() !== '' && $('#3').html() === $('#4').html() && $('#3').html() === $('#5').html()) {
     // location.reload()
+    $('#game-outcome').val('true')
     return console.log('You found a match!')
+  } else {
+    // Populate the outcome input
+    $('#game-outcome').val('false')
   }
 }
 
@@ -36,15 +47,19 @@ let numMoves = 0
 const gameArray = []
 
 const gameBoard = function (event) {
-  // If we're on an even number of moves, place down '0'
+  // If we're on an even number of moves, place down 'X'
   if ($(this).html() === '' && numMoves % 2 === 0 && numMoves < 9) {
     // Fill in slot with 'X' - UI
     $(this).html('X')
     // Store the move in an array
     gameArray[+$(this).attr('id')] = 'X'
     console.log(gameArray)
-    // Populate the game form with value. This will be sent up to the back end through a PATCH AJAX request
-    // $(formid).
+    // Populate the game form with data about the move.
+    // This will be sent up to the back end through a PATCH AJAX request
+    // Populate the index input
+    $('#game-array-index').val(+$(this).attr('id'))
+    // Populate the marker input
+    $('#move-marker').val('X')
     // Add 1 to the numMoves counter
     numMoves = numMoves + 1
     // Check whether this move resulted in a winning combo
@@ -56,8 +71,12 @@ const gameBoard = function (event) {
     // Store the move in an array
     gameArray[+$(this).attr('id')] = 'O'
     console.log(gameArray)
-    // Populate the game form with value. This will be sent up to the back end through a PATCH AJAX request
-    // $(formid).
+    // Populate the game form with data about the move.
+    // This will be sent up to the back end through a PATCH AJAX request
+    // Populate the index input
+    $('#game-array-index').val(+$(this).attr('id'))
+    // Populate the marker input
+    $('#move-marker').val('O')
     // Add 1 to the numMoves counter
     numMoves = numMoves + 1
     // Check whether this move resulted in a winning combo
