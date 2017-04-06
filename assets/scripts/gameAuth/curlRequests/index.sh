824657769f1ae1f41838871a0d2713121ff07461
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-06 11:47:56 -0400

    Wrote a curl request for INDEX action
    This curl request resulted in a 200, OK response

--- assets/scripts/gameAuth/curlRequests/index.sh
+++ assets/scripts/gameAuth/curlRequests/index.sh
@@ -1,35 +1,10 @@
-{
-  "games": [
-    {
-      "id": 1,
-      "cells": ["o","x","o","x","o","x","o","x","o"],
-      "over": true,
-      "player_x": {
-        "id": 1,
-        "email": "and@and.com"
-      },
-      "player_o": {
-        "id": 3,
-        "email": "dna@dna.com"
-      }
-    },
-    {
-      "id": 2,
-      "cells": ["","","","","","","","",""],
-      "over": false,
-      "player_x": {
-        "id": 3,
-        "email": "dna@dna.com"
-      },
-      "player_o": {
-        "id": 1,
-        "email": "and@and.com"
-      }
-    }
-  ]
-}
+#!/bin/bash
+# sh assets/scripts/gameAuth/curlRequests/index.sh
 
-{
-  "games": [
-  ]
-}
+curl "http://localhost:4741/games" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=BAhJIiUwMDk2OThiNDJjYWYzMGVjZjVkOTdkZjc2ZWZhOTY5ZQY6BkVG--d68a5450cfea00f3f8c6e6250dfccad4e9bd6afd"
+
+# data output from curl doesn't have a trailing newline
+echo
